1bfb119f839246b78cd9d70c5c69cc70cfacd309
Victor Levesque <vlevesque@intershop.com.au>
2025-02-17 16:37:50 +1100

    preload

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -1,1 +1,17 @@
 import "@/lib/fontawesome";
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <link rel="preload" href="/images/beach.jpg" as="image" />
+        <link rel="preload" href="/images/lake.jpg" as="image" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
