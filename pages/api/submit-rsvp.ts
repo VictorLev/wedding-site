e751959185610c1f02fe4358893cb483855da611
Victor <levesque.vic@gmail.com>
2026-05-02 18:18:38 -0400

    test

--- pages/api/submit-rsvp.ts
+++ pages/api/submit-rsvp.ts
@@ -1,42 +1,60 @@
 import { google } from 'googleapis';
-import type { NextApiRequest, NextApiResponse } from 'next';
 
-const sheets = google.sheets('v4');
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    const { firstName, lastName, activities, adults, namePlusOne, children, dietaryRestrictions, favoriteSong, comments, stayingOnsite, accommodations  } = req.body;
+    const {
+      firstName,
+      lastName,
+      adults,
+      dietaryRestrictions,
+      favoriteSong,
+      comments,
+      stayingOnsite,
+      accommodations,
+      plusOneFirstName,
+      plusOneLastName,
+    } = body;
 
     const auth = await google.auth.getClient({
       credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_KEY as string),
       scopes: ['https://www.googleapis.com/auth/spreadsheets'],
     });
 
-    const spreadsheetId = process.env.GOOGLE_SHEET_ID;
-    
-    try {
-      await sheets.spreadsheets.values.append({
-        auth: auth,
-        spreadsheetId,
-        range: 'Sheet1!A:A', // Adjust the range as needed
-        valueInputOption: 'USER_ENTERED',
-        insertDataOption: 'INSERT_ROWS',
-        requestBody: {
-          values: [
-            [firstName, lastName, activities.join(', '), adults, namePlusOne, children, dietaryRestrictions, favoriteSong, comments, stayingOnsite, accommodations],
-          ],
-        },
-      });
+    const sheets = google.sheets({ version: 'v4', auth });
 
-      res.status(200).json({ message: 'Form submitted successfully' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Failed to submit form' });
-    }
-  } else {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+    const spreadsheetId = process.env.GOOGLE_SHEET_ID;
+
+    await sheets.spreadsheets.values.append({
+      spreadsheetId,
+      range: 'Sheet1!A:A',
+      valueInputOption: 'USER_ENTERED',
+      requestBody: {
+        values: [
+          [
+            firstName,
+            lastName,
+            adults,
+            plusOneFirstName,
+            plusOneLastName,
+            dietaryRestrictions,
+            favoriteSong,
+            comments,
+            stayingOnsite,
+            accommodations,
+          ],
+        ],
+      },
+    });
+
+    return Response.json({ message: 'Form submitted successfully' });
+  } catch (error) {
+    console.error('RSVP ERROR:', error);
+
+    return Response.json(
+      { message: 'Failed to submit form', error: String(error) },
+      { status: 500 }
+    );
   }
 }
-
-export default handler;
